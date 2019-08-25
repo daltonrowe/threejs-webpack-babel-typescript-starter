8ea14da1040dc5eb5d570067d94d940a2c3070c9
Dalton Rowe <cdaltonrowe@gmail.com>
2019-08-24 21:45:42 -0500

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Typescript, ThreeJS, Webpack, Babel starter
+
+Forked from https://github.com/microsoft/TypeScript-Babel-Starter
+
+- Three JS
+- Babel 7
+- Webpack
+- Typescript
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require("path");
+
+module.exports = {
+  // Change to your "entry-point".
+  entry: "./src/index",
+  mode: "development",
+  watch: true,
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "app.bundle.js"
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".json"]
+  },
+  module: {
+    rules: [
+      {
+        // Include ts, tsx, js, and jsx files.
+        test: /\.(ts|js)x?$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
